e1b5118528a3467bdaece4cd83ad567c6b7e8aa0
Philip Stanislaus <stanislaus.philip@gmail.com>
2018-11-18 14:30:22 +0100

    Add package from NPM

--- gui/src/App.test.tsx
+++ gui/src/App.test.tsx
@@ -2,10 +2,11 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import App from './App'
 import { createStore } from './store/createStore'
+import { initialState } from './store/initialState'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  const store = createStore({})
+  const store = createStore(initialState)
   ReactDOM.render(<App store={store} />, div)
   ReactDOM.unmountComponentAtNode(div)
 })
