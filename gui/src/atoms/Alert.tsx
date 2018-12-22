868672eb37f5ce62a434351682e0ace64339fff8
haneefghanim <haneef.ghanim@gmail.com>
2018-12-21 22:26:15 -0500

    Add swap confirmation alert to the swap page. Add an alert of the private key when a wallet has been created on the account page. Hide private key in input field on the account page.

--- gui/src/atoms/Alert.tsx
+++ gui/src/atoms/Alert.tsx
@@ -1,6 +1,23 @@
+import * as React from 'react'
 import styled from 'styled-components'
 
-export const Alert = styled.div<{ color?: 'success' | 'danger' }>`
+interface IProps {
+  color?: 'success' | 'danger',
+  children?: any,
+  onDismiss?: () => void,
+  style?: any,
+}
+
+export const Alert = ({ color, style, children, onDismiss }: IProps) => (
+  <AlertWrapper>
+    <BaseAlert color={color} style={style}>{children}</BaseAlert>
+    {onDismiss && (
+      <DismissButton onClick={onDismiss}>X</DismissButton>
+    )}
+  </AlertWrapper>
+)
+
+export const BaseAlert = styled.div<{ color?: 'success' | 'danger' }>`
   margin-top: 22px;
   font-family: 'Helvetica';
   font-size: 15px;
@@ -8,4 +25,23 @@ export const Alert = styled.div<{ color?: 'success' | 'danger' }>`
   border-radius: 5px;
   color: #ffffff;
   padding: 22px;
+`
+
+const AlertWrapper = styled.div`
+  position: relative;
+`
+
+const DismissButton = styled.button`
+  border: none;
+  background: transparent;
+  color: #fff;
+  position: absolute;
+  right: 15px;
+  top: 18px;
+  height: 24px;
+  width: 24px;
+  text-align: center;
+  font-size: 14px;
+  padding: 0;
+  cursor: pointer;
 `
